6ecd71935bae410a296222456dddf3b4fdd90cf2
G51DHU <gs_sidhu@hotmail.co.uk>
2022-05-03 22:53:07 +0100

    New endpoint, simplified docstrings.

--- modules/rooms.py
+++ modules/rooms.py
@@ -2,6 +2,7 @@ from typing import List
 from pydantic import BaseModel
 from fastapi import APIRouter
 from pymongo import MongoClient
+from bson.objectid import ObjectId
 from .helper_funcs import *
 
 #
@@ -33,12 +34,25 @@ async def AddRooms(rooms:Rooms):
     return {"Object": {"assigned_id": str(response.inserted_id)}}
 
 
-@router.get("/")
+@router.get("")
 async def AllRooms():
+    """
+        Endpoint, that returns all rooms stored in the database.
+    """
     response = client["rooms"].find({})
     return BsonToJson(response)
 
 @router.get("/by_block")
 async def RoomsByBlock(block_letter:str):
+    """
+        Endpoint, that returns all rooms, located within a certain block stored in the database.
+    """
     response = client["rooms"].find({"name": {"$in": block_letter}})
     return BsonToJson(response)
+
+@router.delete("")
+async def DeleteRooms(room_ids:List[str]):
+    """
+        Endpoint to delete rooms.
+    """
+    client["software"].delete_many({"_id": {"$in": [ObjectId(id) for id in room_ids]}})
