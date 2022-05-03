552e4d4bbd20b7ce168bd2f3cd750c559326cc73
G51DHU <gs_sidhu@hotmail.co.uk>
2022-05-03 22:52:48 +0100

    Simplified docstring.

--- modules/helper_funcs.py
+++ modules/helper_funcs.py
@@ -2,13 +2,8 @@ from bson.json_util import dumps as bson_dumps
 from json import loads as json_loads
 
 def BsonToJson(data):
-    """Converts data that is of type "BSON" into "Json".
-
-    Args:
-        data (bson): Recieves data of type: Bson.
-
-    Returns:
-        json : Returns the data in converted form.
+    """
+        Converts data that is of type "BSON" into "Json".
     """
     parsed_data =  json_loads(bson_dumps(data))
     for index in range(len(parsed_data)):
